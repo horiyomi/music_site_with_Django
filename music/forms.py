8ed6694fedca423aecb5e234edd6a1f2ae4517d4
horiyomi <horiyomi4urluv@outlook.com>
2017-06-24 11:45:29 +0100

    First commit

---
+++ music/forms.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import User
+from .models import Song
+from django import forms
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username','email','password']
+
+# class AddSong(forms.ModelForm)
